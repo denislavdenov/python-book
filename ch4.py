59abe30a612dfbfd998bfe2f9a28216949e7630f
Denov <denov@Denovs-MacBook-Pro.local>
2018-12-10 08:12:09 +0200

    adding ch4

---
+++ ch4.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+print(len('Albert Einstein'))
+
+print('Albert' + ' Einstein')
+
+x = 'Albert'
+y = ' '
+z = 'Einstein'
+
+print(x + y + z)
+print('Albert' + y + z)
+print('Four score and ' + str(7) + ' years ago')
+
+print(4 * '-')
+x = '-'
+print(4 * x)
+print('one\ntwo\nthree')
+print('one\ttwo\nthree\tfour')
+print('one','two','three', sep='.. ')
+print('Enter population:', end='\n')
+population = input()
+print(population)
+population = int(population)
+population = population + 1
+print(population)
+species = input("Please enter a species: ")
+print(species)
+print('''Hello,
+My name is Denislav.
+Nice to meet you.''')
+print('Darwin\'s')
+print('left\\right')
