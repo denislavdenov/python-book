faa40cb96d8afcee7ccce9891d717149956ed64c
Denov <denov@Denovs-MacBook-Pro.local>
2019-01-15 14:40:02 +0200

    add chap12

---
+++ ch12/ch12.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+counts = [809, 834, 477, 478, 307, 122, 96, 102, 324, 476]
+print(min(counts))
+print(counts.index(min(counts)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
